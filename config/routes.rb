f8177b0c6d423b1140a74a13605a8002c8249f5f
Eliza Upadhyaya <eliza.upadhyaya@gmail.com>
2023-01-29 17:19:49 +0000

    cratedrails app that displays Q/A

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # We want to display a page with a <form> to our users at localhost:3000/ask.
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
